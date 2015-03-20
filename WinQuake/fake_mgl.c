e5b0cadb53af8d3bcdfaa92969d9e17c2e9d7772
Azarien <Azarien@users.noreply.github.com>
2015-03-20 01:28:29 +0100

    * some comments added from MGL docs.

--- WinQuake/fake_mgl.c
+++ WinQuake/fake_mgl.c
@@ -46,7 +46,10 @@ void 	FakeMGL_detectGraph(m_int *driver,m_int *mode)
 
 uchar *	FakeMGL_availableModes(void)
 {
-	return MGL_availableModes();
+	// modes are 0xFF-terminated
+	uchar *result = MGL_availableModes();
+//	result[1] = 0xFF;
+	return result;
 }
 
 
@@ -56,15 +59,17 @@ m_int	FakeMGL_modeResolution(m_int mode,m_int *xRes,m_int *yRes,m_int *bitsPerPi
 }
 
 
-bool	FakeMGL_init(m_int *driver,m_int *mode,const char *mglpath)
+bool	FakeMGL_init(m_int *driver,m_int *mode)
 {
-	return MGL_init(driver, mode, mglpath);
+	/* Initialise the MGL for fullscreen output */
+	return MGL_init(driver, mode, "");
 }
 
 
-bool	FakeMGL_initWindowed(const char *mglpath)
+bool	FakeMGL_initWindowed()
 {
-	return MGL_initWindowed(mglpath);
+	/* Initialise the MGL just for Windowed output, not full screen */
+	return MGL_initWindowed("");
 }
 
 
@@ -76,6 +81,10 @@ void	FakeMGL_setSuspendAppCallback(MGL_suspend_cb_t staveState)
 
 bool	FakeMGL_changeDisplayMode(m_int mode)
 {
+	/* Change the active display mode. You must destroy all display device
+	* contexts before calling this function, and re-create them again with
+	* the new display mode. Does not affect any event handling hooks.
+	*/
 	return MGL_changeDisplayMode(mode);
 }
 
@@ -154,6 +163,19 @@ bool	FakeMGL_destroyDC(MGLDC *dc)
 
 void 	FakeMGL_registerFullScreenWindow(HWND hwndFullScreen)
 {
+	/* Function to register a fullscreen window with the MGL. If you wish
+	 * for the MGL to use your own window for fullscreen modes, you can
+	 * register it with this function. Note that when the MGL goes into
+	 * fullscreen modes, the attributes, size and position of the window are
+	 * modified to make it into a fullscreen Window necessary to cover the
+	 * entire desktop, and the state of the window will be restore to the original
+	 * format on return to normal GDI mode.
+	 *
+	 * Note that if you are using a common window for Windowed mode and fullscreen
+	 * modes of your application, you will need to ensure that certain messages
+	 * that you window normally handles in windowed modes are ignored when in
+	 * fullscreen modes.
+	 */
 	MGL_registerFullScreenWindow(hwndFullScreen);
 }
 
@@ -208,11 +230,21 @@ bool	FakeMGL_setWinDC(MGLDC *dc,MGL_HDC hdc)
 
 void	FakeMGL_appActivate(MGLDC *winDC,bool active)
 {
+	/* Let the MGL know when your application is being activated or deactivated.
+	* This function only needs to be called when running in Windowed modes and
+	* you have set the system palette to SYSPAL_NOSTATIC mode, to ensure
+	* that the MGL can properly re-map your application palette when your
+	* app is not active and allow Windows to re-map your bitmap colors on the
+	* fly. This function should be passed a pointer to the currently active
+	* MGL Windowed DC and a flag to indicate whether the app is in the background
+	* or not.   
+	*/
 	MGL_appActivate(winDC, active);
 }
 
 
 bool	FakeMGL_activatePalette(MGLDC *dc,bool unrealize)
 {
+	/* Activate the WindowDC's palette */
 	return MGL_activatePalette(dc, unrealize);
 }
