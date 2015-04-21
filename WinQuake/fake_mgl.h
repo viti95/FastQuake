e8577ff103d5628ff187a49cb0081e89e3404c4a
Azarien <Azarien@users.noreply.github.com>
2015-04-21 22:05:49 +0200

    two DIB MGLDCs now contained in just one Fake MGLDC

--- WinQuake/fake_mgl.h
+++ WinQuake/fake_mgl.h
@@ -1,6 +1,7 @@
 typedef struct 
 {
-	MGLDC *mgldc;
+	MGLDC *mgldca;
+	MGLDC *mgldcb;
 } FakeMGLDC_DIB;
 
 typedef struct 
@@ -32,10 +33,10 @@ bool	FakeMGL_DIB_activatePalette(FakeMGLDC_DIB *dc,bool unrealize);
 m_int 	FakeMGL_DIB_registerDriver(const char *name,void *driver);
 bool	FakeMGL_DIB_initWindowed();
 bool	FakeMGL_DIB_changeDisplayMode(m_int mode);
-bool	FakeMGL_DIB_createWindowedDC(MGL_HWND hwnd, m_int xSize,m_int ySize, FakeMGLDC_DIB **windc, FakeMGLDC_DIB **memdc);
+bool	FakeMGL_DIB_createWindowedDC(MGL_HWND hwnd, m_int xSize,m_int ySize, FakeMGLDC_DIB **dc);
 void 	FakeMGL_DIB_lock(FakeMGLDC_DIB *dc, void **surface, int *bytesPerLine);
 bool	FakeMGL_DIB_setWinDC(FakeMGLDC_DIB *dc,MGL_HDC hdc);
-void 	FakeMGL_DIB_bitBltCoord(FakeMGLDC_DIB *dst,FakeMGLDC_DIB *src,m_int left,m_int top,m_int right,m_int bottom,m_int dstLeft,m_int dstTop,m_int op);
+void 	FakeMGL_DIB_bitBltCoord(FakeMGLDC_DIB *dc,m_int left,m_int top,m_int right,m_int bottom,m_int dstLeft,m_int dstTop,m_int op);
 
 // FULL-specific functions
 bool	FakeMGL_FULL_destroyDC(FakeMGLDC_FULL *dc);
