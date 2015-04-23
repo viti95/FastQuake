3781cbd1041a5c4de44bb8daef689f2b14960f3f
Azarien <Azarien@users.noreply.github.com>
2015-04-23 02:11:33 +0200

    dead code del

--- WinQuake/txt_win.c
+++ WinQuake/txt_win.c
@@ -333,22 +333,6 @@ static inline void UpdateCharacter(int x, int y)
     }
 }
 
-static int LimitToRange(int val, int min, int max)
-{
-    if (val < min)
-    {
-        return min;
-    }
-    else if (val > max)
-    {
-        return max;
-    }
-    else
-    {
-        return val;
-    }
-}
-
 void TXT_UpdateScreen(void)
 {
     SDL_Rect rect;
