bf6438a611e3ebf2a9ed82f0e7aa2d36e87d4c4e
viti95 <victornt95@gmail.com>
2021-06-13 17:39:30 +0200

    Remove multiplayer drivers (smaller executable)

--- WinQuake/net.h
+++ WinQuake/net.h
@@ -152,34 +152,7 @@ extern qsocket_t	*net_activeSockets;
 extern qsocket_t	*net_freeSockets;
 extern int			net_numsockets;
 
-typedef struct
-{
-	char		*name;
-	qboolean	initialized;
-	int			controlSock;
-	int			(*Init) (void);
-	void		(*Shutdown) (void);
-	void		(*Listen) (qboolean state);
-	int 		(*OpenSocket) (int port);
-	int 		(*CloseSocket) (int socket);
-	int 		(*Connect) (int socket, struct qsockaddr *addr);
-	int 		(*CheckNewConnections) (void);
-	int 		(*Read) (int socket, byte *buf, int len, struct qsockaddr *addr);
-	int 		(*Write) (int socket, byte *buf, int len, struct qsockaddr *addr);
-	int 		(*Broadcast) (int socket, byte *buf, int len);
-	char *		(*AddrToString) (struct qsockaddr *addr);
-	int 		(*StringToAddr) (char *string, struct qsockaddr *addr);
-	int 		(*GetSocketAddr) (int socket, struct qsockaddr *addr);
-	int 		(*GetNameFromAddr) (struct qsockaddr *addr, char *name);
-	int 		(*GetAddrFromName) (char *name, struct qsockaddr *addr);
-	int			(*AddrCompare) (struct qsockaddr *addr1, struct qsockaddr *addr2);
-	int			(*GetSocketPort) (struct qsockaddr *addr);
-	int			(*SetSocketPort) (struct qsockaddr *addr, int port);
-} net_landriver_t;
-
-#define	MAX_NET_DRIVERS		8
-extern int 				net_numlandrivers;
-extern net_landriver_t	net_landrivers[MAX_NET_DRIVERS];
+#define	MAX_NET_DRIVERS		1
 
 typedef struct
 {
